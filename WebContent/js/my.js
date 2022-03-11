af7830c942e8c77a69ba50e20ce946b0c34eff60
nadad <nadad@DESKTOP-FMTBGUO>
2022-03-11 11:05:11 +0900

    commit test

---
+++ WebContent/js/my.js
@@ -0,0 +1,62 @@
+//$(document).ready(function(){
+//	const id = $.cookie("id");
+//	if(id){
+//		$("#loginSpan").html( user.id+"님 접속" +"<button id='logoutBtn'>logout</button>");
+//	}
+//	
+//	$("#loginBtn").click(function () {
+//		const id = $("#id").val();
+//		const pw = $("#pw").val();
+//		$.post("http://localhost:8080/0310_HaapyHouse/main", { id, pw, sign:"login" }, function (data) {
+//			const user = JSON.parse(data);
+//			if(user.id){
+//				$.cookie("id",user.id)
+//			}
+////			if(user.id=="ssafy"&& user.pw=="1234"){
+////				alert("로그인 성공");
+////				 $('#loginSpan').html( user.id+"님 접속" +"<button id='logoutBtn'>logout</button>");
+////			} else if(user.id!="ssafy"){
+////				alert("아이디가 잘 못 되었습니다.");
+////				location.reload();
+////			} else{
+////				alert("비밀번호가 잘 못 되었습니다.");
+////				location.reload();
+////			}
+//			});
+//	})
+//	
+//	$(document).on("click","#logoutBtn",function(){
+//		alert("로그아웃!");
+//		location.reload();
+//	})
+//})
+//
+
+$(document).ready(function(){
+   const id = $.cookie("id");
+   if(id){
+      $("#loginSpan").html(id +"<button id ='logoutBtn'>logout</button>")
+   }
+   $(document).on("click", "#logoutBtn", function() {
+      $.post("main",{sign:"logout"},function(){
+         $.removeCookie("id");
+         location.reload();
+      });
+   });
+   $("#loginBtn").click(function() {
+      const id = $("#id").val();
+      const pw = $("#pw").val();
+      
+      $.post("main", {id, pw, sign: "login"}, function(data){
+         //const user = JSON.parse(document.cookie);
+         console.log(data);
+         let obj = JSON.parse(data);
+         console.log(obj);
+         if(obj.id){
+            $.cookie("id", obj.id);
+            $("#loginSpan").html(id +"<button id ='logoutBtn'>logout</button>")
+         }
+      });
+   });
+   
+});
---
+++ src/web/controller/mainServlet.java
@@ -0,0 +1,52 @@
+package web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class mainServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		System.out.println("doPost 호출됨");
+		String sign = request.getParameter("sign");
+		if(sign!=null) {
+			System.out.println(sign);
+			if(sign.equals("login")) {
+				String id = request.getParameter("id");
+				String pw = request.getParameter("pw");
+				System.out.println(id+" : " + pw);
+				
+				// db
+				
+				// 세션 처리
+				HttpSession session = request.getSession();
+				session.setAttribute("id", id);
+				
+				out.print("{\"id\":\""+id+"\"}");
+//				out.append("{\"id\":\""+id+"\", \"pw\":\""+pw+"\"}");
+			} else if(sign.equals("logout")) {
+				HttpSession session = request.getSession();
+				session.invalidate();
+			}
+		} else {
+			// 침해대응코드(해킹당한것이다.)
+			System.out.println("비상 ~!!!"+request.getRemoteAddr());
+			
+		}
+		
+	}
+
+}
