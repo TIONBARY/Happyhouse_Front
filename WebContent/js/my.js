59e357b01f262ac759c9fe48a74777d110ed51ad
김시언 <rlatldjs123@gmail.com>
2022-03-14 13:40:04 +0900

    비밀번호찾기 추가

--- WebContent/js/my.js
+++ WebContent/js/my.js
@@ -3,7 +3,7 @@
 //   if(id){
 //      $("#loginSpan").html( user.id+"님 접속" +"<button id='logoutBtn'>logout</button>");
 //   }
-//   
+//
 //   $("#loginBtn").click(function () {
 //      const id = $("#id").val();
 //      const pw = $("#pw").val();
@@ -24,7 +24,7 @@
 ////         }
 //         });
 //   })
-//   
+//
 //   $(document).on("click","#logoutBtn",function(){
 //      alert("로그아웃!");
 //      location.reload();
@@ -32,98 +32,97 @@
 //})
 //
 
+$(document).ready(function () {
+  const id = $.cookie("id");
+  if (id) {
+    $("#loginSpan").html(id + "<button id ='logoutBtn'>logout</button>");
+  }
 
-$(document).ready(function(){
-   const id = $.cookie("id");
-   if(id){
-      $("#loginSpan").html(id +"<button id ='logoutBtn'>logout</button>")
-   }
-   
-   $(document).on("click", "#logoutBtn", function() {
-      $.post("main",{sign:"logout"},function(){
-         $.removeCookie("id");
-         location.reload();
-      });
-   });
-   
-   $("#loginBtn").click(function() {
-      const id = $("#id").val();
-      const pw = $("#password").val();
-      
-      $.post("main", {id, pw, sign: "login"}, function(data){
-         let obj = JSON.parse(data);
-         let user = JSON.parse(localStorage.getItem(id));
-         if(obj.id == user.id && obj.pw == user.password){
-            $.cookie("id", obj.id);
-            location.href="index.html";
-            $("#loginSpan").html(id +"<button id ='logoutBtn'>logout</button>")
-         }else{
-            alert("로그인 정보를 다시 입력해주세요.");
-         }
-      });
-   });
-   
-   $("#registBtn").click(function(){
-      let id = $("#id").val();
-      let password = $("#password").val();
-      let name = $("#name").val();
-      let email = $("#email").val();
-      let age = $("#age").val();
-      
-      const user  = {
-               id: id,
-               password: password,
-               name: name,
-               email: email,
-               age: age,
-      };
-      
-      localStorage.setItem(id, JSON.stringify(user));
-      alert("회원가입 완료");
-      location.href="index.html";
-   });
-   
-   const user_info=JSON.parse(localStorage.getItem($.cookie("id")));
-   $(".user_info").html("<p>아이디 : <input type=text value="+user_info.id+" id=user_info_id></p>"
-         +"<p>비밀번호 : <input type=password value="+user_info.password+" id=user_info_password></p>"
-         +"<p>이름 : <input type=text value="+user_info.name+" id=user_info_name></p>"
-         +"<p>이메일 : <input type=text value="+user_info.email+" id=user_info_email></p>"
-         +"<p>나이 : <input type=text value="+user_info.age+" id=user_info_age></p>"
-         );
-   
-   $("#user_info_button_ok").click(function () {
-      location.href="index.html";
-   });
- 
-   $("#user_info_button_edit").click(function(){
-	   let id = $("#user_info_id").val();
-	   let password = $("#user_info_password").val();
-	   let name = $("#user_info_name").val()
-	   let email = $("#user_info_email").val();
-	   let age = $("#user_info_age").val();
-	      
-	   const user  = {
-			   id: id,
-	           password: password,
-	           name: name,
-	           email: email,
-	           age: age,
-	   };
-	      
-	   localStorage.setItem(id, JSON.stringify(user));
-	   alert("회원정보 수정");
-	   location.href="index.html";
-   });
-   
-   $("#user_info_button_remove").click(function(){
-	   $.removeCookie("id");
-	   localStorage.removeItem(user_info.id);
-	   location.href="index.html";
-   });
+  $(document).on("click", "#logoutBtn", function () {
+    $.post("main", { sign: "logout" }, function () {
+      $.removeCookie("id");
+      location.reload();
+    });
+  });
 
-   var aptname;
-   $(".aptlistBtn").click(function(){
-	   window.open("detail.html", "poll", "width=420, height=300, top=300, left=400");
-	   aptname = $()
-   });
+  $("#loginBtn").click(function () {
+    const id = $("#id").val();
+    const pw = $("#password").val();
+
+    $.post("main", { id, pw, sign: "login" }, function (data) {
+      let obj = JSON.parse(data);
+      let user = JSON.parse(localStorage.getItem(id));
+      if (obj.id == user.id && obj.pw == user.password) {
+        $.cookie("id", obj.id);
+        location.href = "index.html";
+        $("#loginSpan").html(id + "<button id ='logoutBtn'>logout</button>");
+      } else {
+        alert("로그인 정보를 다시 입력해주세요.");
+      }
+    });
+  });
+
+  $("#findPw").click(function () {
+    let id = $("#id").val();
+    let user = JSON.parse(localStorage.getItem(id));
+
+    alert("비밀번호는 " + user.password + "입니다.");
+  });
+
+  $("#registBtn").click(function () {
+    let id = $("#id").val();
+    let password = $("#password").val();
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let age = $("#age").val();
+
+    const user = {
+      id: id,
+      password: password,
+      name: name,
+      email: email,
+      age: age,
+    };
+
+    localStorage.setItem(id, JSON.stringify(user));
+    alert("회원가입 완료");
+    location.href = "index.html";
+  });
+
+  $("#user_info_button_ok").click(function () {
+    location.href = "index.html";
+  });
+
+  $("#user_info_button_edit").click(function () {
+    let id = $("#user_info_id").val();
+    let password = $("#user_info_password").val();
+    let name = $("#user_info_name").val();
+    let email = $("#user_info_email").val();
+    let age = $("#user_info_age").val();
+
+    const user = {
+      id: id,
+      password: password,
+      name: name,
+      email: email,
+      age: age,
+    };
+
+    localStorage.setItem(id, JSON.stringify(user));
+    alert("회원정보 수정");
+    location.href = "index.html";
+  });
+
+  $("#user_info_button_remove").click(function () {
+    alert(user_info.id + "님 회원탈퇴하셨습니다.");
+    $.removeCookie("id");
+    localStorage.removeItem(user_info.id);
+    location.href = "index.html";
+  });
+
+  var aptname;
+  $(".aptlistBtn").click(function () {
+    window.open("detail.html", "poll", "width=420, height=300, top=300, left=400");
+    aptname = $();
+  });
 });
