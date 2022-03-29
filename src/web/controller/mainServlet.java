b17ec071aca4040c6a40dfdf9d14cd0e3a5d8b04
김시언 <rlatldjs123@gmail.com>
2022-03-29 18:24:54 +0900

    DB연결

--- src/web/controller/mainServlet.java
+++ src/web/controller/mainServlet.java
@@ -9,44 +9,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class mainServlet extends HttpServlet {
+import web.dao.UserDAO;
+import web.util.MyException;
+import web.vo.UserVO;
 
+public class mainServlet extends HttpServlet {
+UserDAO userDAO;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		doPost(request, response);
 	}
-	
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
-		System.out.println("doPost 호출됨");
+
 		String sign = request.getParameter("sign");
-		if(sign!=null) {
-			System.out.println(sign);
+		if (sign != null) {
 			if(sign.equals("login")) {
 				String id = request.getParameter("id");
 				String pw = request.getParameter("pw");
-				System.out.println(id+" : " + pw);
-				
-				// db
-				
-				// 세션 처리
-				HttpSession session = request.getSession();
-				session.setAttribute("id", id);
-				
-				out.print("{\"id\":\""+id+"\", \"pw\":\""+pw+"\"}");
-//				out.append("{\"id\":\""+id+"\", \"pw\":\""+pw+"\"}");
-			} else if(sign.equals("logout")) {
-				HttpSession session = request.getSession();
-				session.invalidate();
+				try {
+					UserVO userVO = new UserVO(id, pw);
+					userDAO = UserDAO.getInstance();
+					userVO = userDAO.login(userVO);
+					if(userVO != null) {
+						HttpSession session = request.getSession();
+						session.setAttribute("login_user", userVO);
+						out.append("ok");
+					}else out.append("fail");
+				} catch (MyException e) {
+					out.append(e.getMessage());
+					e.printStackTrace();
+				}
+			}else if(sign.equals("regist")) {
+				String id = request.getParameter("id");
+				String password = request.getParameter("password");
+				String name = request.getParameter("name");
+				String email = request.getParameter("email");
+				int age = Integer.parseInt(request.getParameter("age"));
+				try {
+					UserVO userVO = new UserVO(id, password, name, email, age);
+					userDAO = UserDAO.getInstance();
+					userDAO.userRegist(userVO);
+					
+					out.append("ok");
+				} catch (MyException e) {
+					out.append(e.getMessage());
+					e.printStackTrace();
+				}
 			}
 		} else {
-			// 침해대응코드(해킹당한것이다.)
-			System.out.println("비상 ~!!!"+request.getRemoteAddr());
-			
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
 		}
-		
 	}
-
 }
--- src/web/dao/UserDAO.java
+++ src/web/dao/UserDAO.java
@@ -1,7 +1,74 @@
 package web.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
+
+import web.util.MyException;
+import web.vo.UserVO;
 
 public class UserDAO {
 	DataSource ds;
+	private static UserDAO instance;
+	
+	public static UserDAO getInstance() {
+		if(instance==null) {
+			instance = new UserDAO();
+		}
+		return instance;
+	}
+
+	private UserDAO() {
+		try {
+			Context ic = new InitialContext();
+			Context ic2 = (Context)ic.lookup("java:comp/env");
+			ds = (DataSource)ic2.lookup("pjtDB");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void userRegist(UserVO userVO) throws MyException{
+		try(Connection con = ds.getConnection()){
+			String sql = "insert into user(id, pw, email, name, age) values(?, ?, ?, ?, ?)";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, userVO.getId());
+			stmt.setString(2, userVO.getPw());
+			stmt.setString(3, userVO.getEmail());
+			stmt.setString(4, userVO.getName());
+			stmt.setInt(5, userVO.getAge());
+			
+			int i = stmt.executeUpdate();
+			System.out.println(i+"행이 insert되었습니다. ");
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw new MyException("회원가입 실패");
+		}
+
+	}
+
+	public UserVO login(UserVO userVO) throws MyException{
+		try(Connection con = ds.getConnection()){
+			String sql = "select name from user where id=? and pw=? ";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, userVO.getId());
+			stmt.setString(2, userVO.getPw());
+			
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				userVO.setName(rs.getString("name"));
+				return userVO;
+			}
+			return null;
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw new MyException("login 실패");
+		}
+	}
 }
